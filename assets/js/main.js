8a814c4ff25532478e65acd1ef3551da36ff1927
Jayesh Patil <patiljayesh636261@gmail.com>
2026-01-25 17:07:05 +0530

    added few files and made a default user

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+//this is for checking and stuff
+const defaultUser ={
+    progress :0,
+    badges:[],
+    completedModules:[],
+    scoresOfQuiz:{
+        income:0,
+        investment:0,
+        insurance:0,
+        tax:0,
+        scams:0,
+    }
+}
